31ea31756209b19b9c03d65adef75abf4e50b230
Rachel Cartwright <stormchica@gmail.com>
2014-06-05 16:24:39 -0700

    1st initial commit
    rails new BootstrapRailsBlog -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootstrapRailsBlog::Application.config.secret_key_base = '1e37c5ef2c2fb0cd8faf434cb912a0cf9e09ebf5147eddcdb6f654f89c7d2e8ca7dde1b9a8a450cffe492d7b1732375ec49bf84bfb1b51b2530383328ba5f346'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BootstrapRailsBlog::Application.config.session_store :cookie_store, key: '_BootstrapRailsBlog_session'
