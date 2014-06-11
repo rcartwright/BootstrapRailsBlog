0740e682fe2419a0a84633c4116159e8e1b6937b
Rachel Cartwright <stormchica@gmail.com>
2014-06-11 14:49:27 -0700

    set root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 BootstrapRailsBlog::Application.routes.draw do
+root 'posts#index'
+
   resources :posts do
     resources :comments, :only => [:create]
   end
+	
+	
 	
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
